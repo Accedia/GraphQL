bd48f13e668999bbdfaa2f6df3b3358f79247bf4
Stefan Stefanov <stefan.stefanov@accedia.com>
2019-09-25 08:34:40 +0300

    Clean Master

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import { GraphQLServer, PubSub } from 'graphql-yoga'
+import { GraphQLServer } from 'graphql-yoga'
 import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-
-const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
-        Query,
-        Mutation,
-        Subscription
     },
     context: {
         db
--- src/resolvers/Query.js
+++
@@ -1,3 +0,0 @@
-const Query = { }
-
-export default Query
